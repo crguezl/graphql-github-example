928fcc511cd9b51c9bb613f661d0d4f9af6fa2af
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-11-06 15:22:11 +0000

    page info

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ const numRepos = Number(process.argv[2]) || 10;
 
 const handler = (res, r) => {
   //console.log(r);
-  const repos = r.data.search.edges;
-  const repoCount = r.data.search.repositoryCount
-  if (repos.length) {
-    let lastCursor = repos[repos.length-1].cursor;
-    const data = JSON.stringify(r, null, 2);
-    console.log(data)
+  const result = r.data.search;
+  const repos = result.edges;
+  const repoCount = result.repositoryCount
+  if (result.pageInfo.hasNextPage) {
+    let lastCursor = result.pageInfo.endCursor; //repos[repos.length - 1].cursor;
+    // const data = JSON.stringify(r, null, 2);
+    // console.log(data)
     console.log(`lastCursor: <${lastCursor}>`)
     console.log(`base64.decode(lastCursor): <${base64.decode(lastCursor)}>`)
     // Tricky!! See [How to search specific page through GitHub API V4](https://stackoverflow.com/questions/64115904/how-to-search-specific-page-through-github-api-v4) in StackOverflow
-    let offset = Number(base64.decode(lastCursor).replace(/^cursor:/,''))-repos.length+1
+    let offset = Number(base64.decode(lastCursor).replace(/^cursor:/, '')) - repos.length + 1
     console.log(`offset=${offset}`)
-    console.log(`base64.encode('cursor:'+numRepos+repos.length-1): ${base64.encode('cursor:'+(numRepos+repos.length-1))}`)
-  
-    res.render('pages/index',{ repos: repos, lastCursor: lastCursor, offset: offset });
+    console.log(`base64.encode('cursor:'+numRepos+repos.length-1): ${base64.encode('cursor:' + (numRepos + repos.length - 1))}`)
+
+    res.render('pages/index', { repos: repos, lastCursor: lastCursor, offset: offset });
     console.log(`repoCount=${repoCount}`)
   } else {
-      console.log('No more repos found!');
-      console.log(`Array came empty repoCount=${repoCount}`)
+    console.log('No more repos found!');
+    console.log(`Array came empty repoCount=${repoCount}`)
   }
 }
 
@@ -33,6 +34,10 @@ const firstQuery = gql`
 {
   search(query: "topic:gh-extension sort:stars", type: REPOSITORY, first: ${numRepos} ) {
     repositoryCount
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
     edges {
       cursor
       node {
@@ -54,6 +59,10 @@ const subsequentQueries = (cursor) => gql`
       {
         search(query: "topic:gh-extension sort:stars", type: REPOSITORY, first: ${numRepos}, after: "${cursor}") {
           repositoryCount
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
           edges {
             cursor
             node {
@@ -74,8 +83,8 @@ const subsequentQueries = (cursor) => gql`
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://api.github.com/graphql", 
-    fetch, 
+    uri: "https://api.github.com/graphql",
+    fetch,
     headers: { 'Authorization': `Bearer ${process.env['GITHUB_TOKEN']}`, },
   }),
   cache
@@ -83,22 +92,22 @@ const client = new ApolloClient({
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   client
-    .query({query: firstQuery})
-    .then(r => handler(res,r))
+    .query({ query: firstQuery })
+    .then(r => handler(res, r))
     .catch(error => console.error(error))
 });
 
-app.get('/next/:cursor', function(req, res) {
+app.get('/next/:cursor', function (req, res) {
   client
-    .query({ query: subsequentQueries(req.params.cursor)})
-    .then(r => handler(res,r))
+    .query({ query: subsequentQueries(req.params.cursor) })
+    .then(r => handler(res, r))
     .catch(error => console.error(error))
 });
 
 // about page
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
   res.render('pages/about');
 });
 
